8e2dcd3dda2c5c4aeb03b2245211b65455e52c5d
MartinianoAlbo <alvaro.al1493@gmail.com>
2022-08-24 21:08:26 -0300

    modify navigate

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,5 @@
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
   NavLink
 } from 'react-router-dom';
@@ -16,20 +15,20 @@ export const Navigation = () => {
             <img src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Shopping</NavLink>
+              <NavLink to="/" className="nav-active">Shopping</NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink to="/about" className="nav-active">About</NavLink>
             </li>
             <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink to="/users" className="nav-active">Users</NavLink>
             </li>
           </ul>
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
+        {/* <Switch> */}
           <Route path="/about">
             <h1>About</h1>
           </Route>
@@ -39,7 +38,7 @@ export const Navigation = () => {
           <Route path="/">
             <ShoppingPage />
           </Route>
-        </Switch>
+        {/* </Switch> */}
       </div>
     </Router>
   );
