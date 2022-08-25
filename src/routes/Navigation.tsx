29a671fd89a3d87c372d9eea4adfc0a40c975764
MartinianoAlbo <alvaro.al1493@gmail.com>
2022-08-24 21:17:10 -0300

    update routes react router dom

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,6 @@
 import {
   BrowserRouter as Router,
+  Routes,
   Route,
   NavLink
 } from 'react-router-dom';
@@ -15,30 +16,29 @@ export const Navigation = () => {
             <img src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" className="nav-active">Shopping</NavLink>
+              <NavLink to="/"  className={({isActive}) => isActive? 'nav-active' : undefined}>Shopping</NavLink>
             </li>
             <li>
-              <NavLink to="/about" className="nav-active">About</NavLink>
+              <NavLink to="/about" className={({isActive}) => isActive? 'nav-active' : undefined}>About</NavLink>
             </li>
             <li>
-              <NavLink to="/users" className="nav-active">Users</NavLink>
+              <NavLink to="/users" className={({isActive}) => isActive? 'nav-active' : undefined}>Users</NavLink>
             </li>
           </ul>
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        {/* <Switch> */}
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <ShoppingPage />
-          </Route>
-        {/* </Switch> */}
+        <Routes>
+          <Route path="/about"  element={<h1>About</h1>} />
+            
+          
+          <Route path="/users" element={<h1>Users</h1>} />
+            
+          
+          <Route path="/" element={ <ShoppingPage />}/>
+           
+        </Routes>
       </div>
     </Router>
   );
